14257c147d0ac93ff3d4b70fbeb10c81e8453499
Bernice Mbayo <bernicembayo7@gmail.com>
2022-09-30 12:25:08 +0200

    list

---
+++ android/app/src/main/kotlin/com/example/corsegit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.corsegit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Articles')),
+      body: Column(
+        children: const [
+          ListTile(
+            title: Text('chaussure'),
+          )
+        ],
+      ),
+    );
+  }
+}
